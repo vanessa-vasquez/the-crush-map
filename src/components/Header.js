097ca0cc0ce27c46df86ea57cadfdbaa52c17c6e
Vanessa <vvasquez528@gmail.com>
2022-08-23 22:47:08 -0400

    Make login page responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 import '../styles/Header.css';
 
-import logo from '../assets/crush_map_logo.svg';
+import web_logo from '../assets/crush_map_logo.svg';
+import mobile_logo from '../assets/crush_heart.svg';
 
 export default function Header() {
-  document.body.style.backgroundColor = 'white';
-
   return (
     <div className="header-container">
-      <div>
-        <Image src={logo} className="logo" fluid="true" />
+      <div className="d-block d-lg-none">
+        <Image src={mobile_logo} className="logo" fluid="true" />
+      </div>
+      <div className="d-none d-lg-block">
+        <Image src={web_logo} className="logo" fluid="true" />
       </div>
       <div className="header-text">
         <div className="title">the crush map.</div>
-        <div className="desc">
-          A graph visualization of student <br />
-          crushes in the CU community
+        <div className="d-none d-lg-block desc">
+          A graph visualization of student crushes in the CU community
         </div>
       </div>
     </div>
